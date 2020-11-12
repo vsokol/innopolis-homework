fa05c69a61cf3d29eb4a88e99efada0d10f4f5b9
Vladimir Sokolov <vsokol74@main.ru>
2020-11-12 10:22:59 +0300

    homework 19 каркас

---
+++ src/main/java/store/sokolov/innopolis/homework_19/task_1_2/Main.java
@@ -0,0 +1,10 @@
+package store.sokolov.innopolis.homework_19.task_1_2;
+
+/**
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Домашнее задание 19");
+    }
+}
